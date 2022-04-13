8f053630fe99d227e0406916bbf352a2ad0a400e
FarrelAustrin <farrelrahmadi@gmail.com>
2022-04-13 10:27:04 +0700

    second foreskin

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -83,12 +83,11 @@ class Booking extends CI_Controller
 
             redirect(base_url() . 'home');
         }
-
-        //
+        //membuat tabel temp jika belum ada
         $this->ModelBooking->createTemp();
         $this->ModelBooking->insertData('temp', $isi);
+        //pesan ketika berhasil memasukkan buku ke keranjang
 
-        //
         $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Buku berhasil ditambahkan ke keranjang </div>');
 
         redirect(base_url() . 'home');
@@ -96,60 +95,53 @@ class Booking extends CI_Controller
 
     public function hapusbooking()
     {
-            $id_buku = $this->uri->segment(3);
-            $id_user = $this->session->userdata('id_user');
+        $id_buku = $this->uri->segment(3);
+        $id_user = $this->session->userdata('id_user');
+        $this->ModelBooking->deleteData(['id_buku' => $id_buku], 'temp');
+        $kosong = $this->db->query("select*from temp where id_user='$id_user'")->num_rows();
+        if ($kosong < 1) {
 
-            $this->ModelBooking->deleteData(['id_buku' => $id_buku], 'temp');
-            $kosong = $this->db->query("select*from temp where id_user='$id_user'")->num_rows();
+            $this->session->set_flashdata('pesan', '<div class="alert alert-massege alert-danger" role="alert">Tidak Ada Buku dikeranjang</div>');
 
-    if ($kosong < 1) {
+            redirect(base_url());
+        } else {
+            redirect(base_url() . 'booking');
+        }
+        $this->ModelBooking->deleteData(['id_buku' => $id_buku], 'temp');
+        $kosong = $this->db->query("select*from temp where id_user='$id_user'")->num_rows();
+        if ($kosong < 1) {
         $this->session->set_flashdata('pesan', '<div class="alert alert-massege alert-danger" role="alert">Tidak Ada Buku dikeranjang</div>');
-
         redirect(base_url());
-    } else {
+        } else {
         redirect(base_url() . 'booking');
+        }
     }
-    $this->ModelBooking->deleteData(['id_buku' => $id_buku], 'temp');
-            $kosong = $this->db->query("select*from temp where id_user='$id_user'")->num_rows();
-
-            if ($kosong < 1) {
-                $this->session->set_flashdata('pesan', '<div class="alert alert-massege alert-danger" role="alert">Tidak Ada Buku dikeranjang</div>');
-                redirect(base_url());
-            } else {
-                redirect(base_url() . 'booking');
-    }
-}////this is important
-    public function bookingSelesai($where)
-    {
+    public function bookingSelesai($where) {
         $this->db->query("UPDATE buku, temp SET buku.dibooking=buku.dibooking+1, buku.stok=buku.stok-1 WHERE buku.id=temp.id_buku");
-
-                $tglsekarang = date('Y-m-d');
-                $isibooking = [
-                    'id_booking' => $this->ModelBooking->kodeOtomatis('booking', 'id_booking'),
-                    'tgl_booking' => date('Y-m-d H:m:s'),
-                    'batas_ambil' => date ('Y-m-d', strtotime('+2 days', strtotime($tglsekarang))),
-                    'id_user' => $where
-                ];
-                
-                $this->ModelBooking->insertData('booking', $isibooking);
-                $this->ModelBooking->simpanDetail($where);
-                $this->ModelBooking->kosongkanData('temp');
-
-                redirect(base_url() . 'booking/info');
+        $tglsekarang = date('Y-m-d');
+        $isibooking = [
+        'id_booking' => $this->ModelBooking->kodeOtomatis('booking', 'id_booking'),
+        'tgl_booking' => date('Y-m-d H:m:s'),
+        'batas_ambil' => date('Y-m-d', strtotime('+2 days', strtotime($tglsekarang))),'id_user' => $where
+        ];
+        //menyimpan ke tabel booking dan detail booking, dan mengosongkan tabel temporari
+        $this->ModelBooking->insertData('booking', $isibooking);
+        $this->ModelBooking->simpanDetail($where);
+        $this->ModelBooking->kosongkanData('temp');
+        redirect(base_url() . 'booking/info');
     }
 
     public function info()
     {
-        $where = $this->session->userdata('id_user');
-        $data['user'] = $this->session->userdata('nama');
-        $data['judul'] = "Selesai Booking";
-        $data['useraktif'] = $this->ModelUser->cekData(['id' => $this->session->userdata('id_user')])->result();
-        $data['items'] = $this->db->query("select*from booking bo, booking_detail d, buku bu where d.id_booking=bo.id_booking and d.id_buku=bu.id and bo.id_user='$where'")->result_array();
-
-        $this->load->view('templates/templates-user/header', $data);
-        $this->load->view('booking/info-booking', $data);
-        $this->load->view('templates/templates-user/modal');
-        $this->load->view('templates/templates-user/footer');
+    $where = $this->session->userdata('id_user');
+    $data['user'] = $this->session->userdata('nama');
+    $data['judul'] = "Selesai Booking";
+    $data['useraktif'] = $this->ModelUser->cekData(['id' => $this->session->userdata('id_user')])->result();
+    $data['items'] = $this->db->query("select*from booking bo, booking_detail d, buku bu where d.id_booking=bo.id_booking and d.id_buku=bu.id and bo.id_user='$where'")->result_array();
+    $this->load->view('templates/templates-user/header', $data);
+    $this->load->view('booking/info-booking', $data);
+    $this->load->view('templates/templates-user/modal');
+    $this->load->view('templates/templates-user/footer');
     }
 
     public function exportToPdf()
@@ -159,20 +151,16 @@ class Booking extends CI_Controller
         $data['judul'] = "Cetak Bukti Booking";
         $data['useraktif'] = $this->ModelUser->cekData(['id' => $this->session->userdata('id_user')])->result();
         $data['items'] = $this->db->query("select*from booking bo, booking_detail d, buku bu where d.id_booking=bo.id_booking and d.id_buku=bu.id and bo.id_user='$id_user'")->result_array();
- 
-    $this->load->library('dompdf_gen');
-
-    $this->load->view('booking/bukti-pdf', $data);
- 
-    $paper_size = 'A4'; // ukuran kertas
-    $orientation = 'landscape'; //tipe format kertas potrait atau landscape
-    $html = $this->output->get_output();
- 
-    $this->dompdf->set_paper($paper_size, $orientation);
-    //Convert to PDF
-    $this->dompdf->load_html($html);
-    $this->dompdf->render();
-    $this->dompdf->stream("bukti-booking-$id_user.pdf", array('Attachment' => 0));
-    // nama file pdf yang di hasilkan
+        $this->load->library('Pdfgenerator');
+        $this->load->view('booking/bukti-pdf', $data);
+        $paper_size = 'A4'; // ukuran kertas
+        $orientation = 'landscape'; //tipe format kertas potrait atau landscape
+        $html = $this->output->get_output();
+        $this->dompdf->setpaper($paper_size, $orientation);
+        //Convert to PDF
+        $this->dompdf->load_html($html);
+        $this->dompdf->render();
+        $this->dompdf->stream("bukti-booking-$id_user.pdf", array('Attachment' => 0));
+        // nama file pdf yang di hasilkan
     }
 }
--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -24,23 +24,19 @@
 
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
 
-                    <a class="nav-item nav-link active" href="<?= base_url(); ?>">Beranda
-                    <span class="sr-only">(current)</span></a>
-                                <?php
+                    <a class="nav-item nav-link active" 
+                    href="<?= base_url(); ?>">Beranda <span class="sr-only">(current)</span></a>
+
+                    <?php
                     if (!empty($this->session->userdata('email'))) { ?>
-                        <a class="nav-item nav-link" href="<?=base_url ('booking'); ?>">Booking <b><?= $this->ModelBooking->getDataWhere('temp', ['email_user' => $this->session->userdata('email')])->num_rows(); ?></b> Buku </a>
-                        <a class="nav-item nav-link" onclick="information('Waktu Pengambilan Buku 1x24 jam dari Booking!!!')" href="<?php echo base_url() . 'booking/exportToPdf/' . $this->session->userdata('id_user'); ?>"Cetak Bukti Booking</a>
+                        <a class="nav-item nav-link" href="<?= base_url('booking');?>">Booking <b><?php $this->ModelBooking->getDataWhere('temp', ['email_user' => $this->session->userdata('email_user')])->num_rows(); ?></b>Buku</a>
 
+                        <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
 
-                        <a class="nav-item nav-
-                        link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
-
-                        <a class="nav-item nav-
-                        link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-
-                        login"></i> Log out</a>
+                        <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log out</a>
 
                         <?php } else { ?>
 
